6f93084ea2122d26a17258bfdfa1247b83a6aa9d
Shiam Sharif <shiam.sharif.07@gmail.com>
2024-02-09 22:10:04 +0600

    commit

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,3 +1,6 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
+
 interface CampProps{
   backgroundImage: string;
   title: string;
@@ -9,7 +12,40 @@ interface CampProps{
 const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps ) => {
   return (
     <div className= {`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}>
-      Camp Site 1
+      <div className="flex h-full flex-col items-start justify-between p-6 
+      lg:px-20 lg:py-10">
+        <div className="flexCenter gap-4">
+          <div className="rounded-full bg-green-50 p-4">
+            <Image 
+              src="/folded-map.svg"
+              alt="map"
+              width={28}
+              height={28}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <h4 className="bold-18 text-white">{title}</h4>
+            <p className="regular-14 text-white">{subtitle}</p>
+          </div>
+        </div>
+
+        <div className="flexCenter gap-6">
+          <span className="flex -space-x-4 overflow-hidden">
+            {PEOPLE_URL.map((url) => (
+              <Image 
+                className="inline-block h-10 w-10 rounded-full"
+                src={url}
+                key={url}
+                alt="person"
+                width={52}
+                height={52}
+              />
+            ))}
+          </span>
+          <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+        </div>
+      </div>
     </div>
   )
 }
@@ -27,10 +63,10 @@ const Camp = () => {
         />
 
         <CampSite 
-          backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
-          peopleJoined="50+ Joined"
+          backgroundImage="bg-bg-img-2"
+          title="Mountain Truno Camp"
+          subtitle="Somewhere in the wildness"
+          peopleJoined="59+ Joined"
         />
       </div>
     </section>
